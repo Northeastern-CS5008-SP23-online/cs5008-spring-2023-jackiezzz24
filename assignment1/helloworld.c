01be0077f240eaffbedcdf093b230c593b637c2a
jackiezzz24 <jackiezzzzz@login-students.khoury.northeastern.edu>
2023-01-11 21:53:03 -0500

    upload assignment1

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-// name: Brent Hailpern
-// email: b.hailpern@northeastern.edu
+// name: Jiaqi Zhao
+// email: zhao.jiaqi2@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is Brent.\n");
+  printf("Hello World.  This is Jiaqi.\n");
   return 0;
 }
