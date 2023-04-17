50976a275598bf0a3143c3cffae09af01a0ea1af
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-04-16 20:16:30 -0700

    upload lab12

--- labassignment12/lab12.c
+++ labassignment12/lab12.c
@@ -1,5 +1,5 @@
-//enter your email here
-//enter your name here
+//Jiaqi Zhao
+//zhao.jiaqi2@northeastern.edu
 
 #include<stdio.h>
 #include<string.h>
@@ -10,9 +10,43 @@ int compression(char arr[], int n, char res[]){
     
     
     //insert your code here
+    int i;
+    if (n < 2) {
+        for (i = 0; i < n; i++) {
+            printf("%c, arr[i]");
+        }
+        return n;
+    }
     
-    
-    
+    //sprintf - convert int to char
+    int j = 1;
+    int count = 1;
+    int index = 0; 
+    char r[10];
+    char curr = arr[0];
+    while (j <= n) {
+        if (arr[j] != curr) {
+            res[index++] = curr;
+            if (count != 1) {
+                sprintf(r, "%d", count);
+                for (i = 0; i < strlen(r); i++) {
+                    res[index++] = r[i];
+                }
+            }
+            count = 1;
+        }
+        else {
+            count++;
+        }
+        curr = arr[j++];
+    }
+    count = 0;
+    for (i = 0; i < index; i++) {
+        printf("%c", res[i]);
+        count += 1;
+    }
+    printf("\n");
+    return count;
     
     
 }
