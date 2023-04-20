b3507bbeffeffc955e5151a768d0bc49c1d31807
Jiaqi Zhao <jiaqi.zhao24@gmail.com>
2023-04-19 17:37:51 -0700

    upload lab 13

--- labassignment13/lab13.c
+++ labassignment13/lab13.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
- #define CHAR_SIZE 26
+#define CHAR_SIZE 26
  
 // Data structure to store a Trie node
 typedef struct Trie
@@ -12,15 +12,31 @@ typedef struct Trie
 }trienode;
  
 // Function that create a new Trie node
-trienode* NewNode()
+trienode* newNode()
 {
-    
+    trienode* newNode = (trienode*) malloc(sizeof(trienode));
+    newNode -> leafnode = 0;
+    int i;
+    for (i = 0; i < CHAR_SIZE; i++) {
+        newNode -> character[i] = NULL;
+    }
+    return newNode;
 }
  
 // Iterative function to insert a string into a Trie
 void insert(trienode *head, char* str)
 {
-    
+    // str->word you have to insert car c = 99
+    trienode* curr = head;
+    int i, index;
+    for (i = 0; i < strlen(str); i++) {
+        index = str[i] - 'a';
+        if (!curr -> character[index]) {
+            curr -> character[index] = newNode();
+        }
+        curr = curr -> character[index];
+    }
+    curr -> leafnode = 1;
 }
 
 
@@ -28,15 +44,33 @@ void insert(trienode *head, char* str)
 // if the string is found in the Trie; otherwise,returns 0.
 int search(trienode* head, char* str)
 {
-    
+    trienode* curr = head;
+    int i, index;
+    for (i = 0; i < strlen(str); i++) {
+        index = str[i] - 'a';
+        if (!curr -> character[index]) {
+            return 0;
+        }
+        curr = curr -> character[index];
+    }
+    return curr != NULL && curr -> leafnode;
 }
     
 
 int startswith(trienode* head,char *str)
 {
-    
-
+    trienode* curr = head;
+    int i, index;
+    for (i = 0; i < strlen(str); i++) {
+        index = str[i] - 'a';
+        if (!curr -> character[index]) {
+            return 0;
+        }
+        curr = curr -> character[index];
+    }
+    return curr != NULL;
 }
+
 void triefree(trienode *head)
 {
     trienode *curr=head;
@@ -53,10 +87,22 @@ void triefree(trienode *head)
 // Trie implementation in C – Insertion, Searching and startswith
 int main()
 {
-    
+    trienode* head = newNode();
  
+    insert(head, "car");
+    printf("%d ", search(head, "car"));       // print 1 if the word "car" is present
+ 
+    insert(head, "cat");
+    printf("%d ", search(head, "cat"));  // print 1 if the word "cat" is present
+    printf("%d ",startswith(head,"ca"));
     
+    insert(head, "do");
+    printf("%d ", search(head, "do"));        // print 1 if the word "do" is present
+    insert(head, "don");
+    printf("%d \n", search(head, "don"));        //print 1 if don is present
+    triefree(head);
     return 0;
+
 }
 
     
--- labassignment13/longestcommonprefix.c
+++ labassignment13/longestcommonprefix.c
@@ -1,24 +1,36 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
-void longestCommonPrefix(char arr[][20], char res[20], int m){
+void longestCommonPrefix(char arr[][20], char res[20],int m){
     int index=0;
+    int i, j;
     int n=strlen(arr[0]);
     //traverse through all characters of first word
-    for(int i=0;i<n;i++){
+    for(i=0;i<n;i++){
         char c=arr[0][i];
         bool flag=true;
     //for comparing c with the rest of strings
-    //insert your code here
+    for (j = 1; j < m; j++) {
+        if (arr[j][i] != c) {
+            flag = false;
+            break;
+        }
+    }
+     if (flag) {
+        res[index++] = c;
+     }   else {
+        break;
+     }
+     res[index] = '\0';
     
-    
+    }
 }
 
-int main(){
+int main() {
     int i;
     char arr[5][20]={"aaabc","aaanv","aaamk","aaavf","aaacfb"};//input array
-    int m=5;
     char res[20];//Array to store the longest common prefix 
+    int m = sizeof(arr)/sizeof(arr[0]);
     longestCommonPrefix(arr,res,m);
     printf("The Longest common prefix is ");
     for(i=0;i<strlen(res);i++){
@@ -27,4 +39,3 @@ int main(){
     printf("\n");
     return 0;
 }
-
